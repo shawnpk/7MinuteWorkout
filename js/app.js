a55bb5e0879485a49dff3ee5a1750797337b5d9e
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-11-21 09:19:39 -0800

    Initialize repository

---
+++ js/app.js
@@ -0,0 +1,18 @@
+angular.module('7minWorkout', []);
+angular.module('app', ['ngRoute', '7minWorkout'])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/start', {
+        templateUrl: 'partials/start.html'
+      })
+      .when('/workout', {
+        templateUrl: 'partials/workout.html',
+        controller: 'WorkoutController'
+      })
+      .when('/finish', {
+        templateUrl: 'partials/finish.html'
+      })
+      .otherwise({
+        redirectTo: '/start'
+      });
+  });
